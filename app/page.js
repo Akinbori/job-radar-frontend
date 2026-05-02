b870c8c21a83b4288fd237bc61d00b315c0d3fec
Akinbori <87369242+Akinbori@users.noreply.github.com>
2026-05-02 12:41:35 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,37 +1,24 @@
-"use client";
+lexport default async function Page() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/opportunities`, {
+    cache: "no-store",
+  });
 
-import { useEffect, useState } from "react";
-
-export default function Page() {
-  const [jobs, setJobs] = useState([]);
-
-  useEffect(() => {
-  fetch(`${process.env.NEXT_PUBLIC_API_URL}/opportunities`)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-      setJobs(data.opportunities || data);
-    })
-    .catch((err) => console.error(err));
-}, []);
+  const data = await res.json();
+  const jobs = data.opportunities || data;
 
   return (
     <div style={{ padding: 20 }}>
       <h1>Job Radar</h1>
 
-      {jobs.length === 0 ? (
-        <p>Loading jobs...</p>
-      ) : (
-        jobs.map((job, i) => (
-          <div key={i} style={{ marginBottom: 20 }}>
-            <h3>{job.title}</h3>
-            <p>{job.company}</p>
-            <a href={job.link} target="_blank">
-              Apply
-            </a>
-          </div>
-        ))
-      )}
+      {jobs.map((job, i) => (
+        <div key={i} style={{ marginBottom: 20 }}>
+          <h3>{job.job_title}</h3>
+          <p>{job.company}</p>
+          <a href={job.job_url} target="_blank">
+            Apply
+          </a>
+        </div>
+      ))}
     </div>
   );
 }
